62a182730039418abc579842d76ee68e702494b4
AnujJha88 <anujjha8788@gmail.com>
2026-09-05 17:05:02 +0530

    Refresh cached Studio shell

--- components/shell/service-worker.tsx
+++ components/shell/service-worker.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export function ServiceWorker() {
   useEffect(() => {
-    if ("serviceWorker" in navigator) navigator.serviceWorker.register("/sw.js?v=10").catch(() => undefined);
+    if ("serviceWorker" in navigator) navigator.serviceWorker.register("/sw.js?v=11").catch(() => undefined);
   }, []);
 
   return null;
